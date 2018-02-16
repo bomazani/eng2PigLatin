8bff2b95ac85bb8b5ea0ab60a09e3cca9f1c2400
Bob Hunt <bob.hunt.email@gmail.com>
2018-02-16 11:06:25 -0500

    initial commit

---
+++ e2PigLat.js
@@ -0,0 +1,8 @@
+const english = "This is a sample script for translating English into Pig Latin."
+const englishWords=english.slice(" ");
+const x=2;
+document.write(x);
+console.log(x);
+console.log(english);
+console.log(englishWords);
+document.write(english);
