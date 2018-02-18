426097b5715df317f2fc1c9eaa51c2617dcd5bf2
Bob Hunt <bob.hunt.email@gmail.com>
2018-02-18 15:09:42 -0500

    additional updates... don't recall what.

--- e2PigLat.js
+++ e2PigLat.js
@@ -1,8 +1,22 @@
+const eng = document.createElement("div");
 const english = "This is a sample script for translating English into Pig Latin."
-const englishWords=english.slice(" ");
-const x=2;
-document.write(x);
-console.log(x);
-console.log(english);
-console.log(englishWords);
-document.write(english);
+const englishWords=english.split([" "]);
+eng.textContent = english;
+document.getElementById("div1").appendChild(eng);
+for(i=0; i<englishWords.length; i++){
+    // console.log(englishWords.length);
+    // console.log(englishWords[i]);
+    // let englishLetters =(englishWords[i].split([""]));
+    
+// ** Need to remove Captial letters for vowel recognition **
+    englishWords[i]=englishWords[i].toLowerCase();
+    // console.log(englishWords[i].charAt(0))
+    // console.log(englishLetters);
+    // console.log(englishLetters[0]);
+    if(englishWords[i].charAt(0)=="a"||englishWords[0]=="e"||englishWords[0]=="i"||englishWords[0]=="o"||englishWords[0]=="U"){
+// if(englishLetters[0]=="a"||englishLetters[0]=="e"||englishLetters[0]=="i"||englishLetters[0]=="o"||englishLetters[0]=="U"){
+    // console.log(englishLetters);
+    // console.log(englishLetters[0])
+    console.log(englishWords[i].search("o"));
+}
+}    
